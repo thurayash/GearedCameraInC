66550ea59dfcb9863bc7e9b7d2f70d099259d66a
karim abou dargham <karim.abou-dargham@epita.fr>
2021-02-21 17:21:19 +0100

    first commit

---
+++ ycbcr_to_BW.c
@@ -0,0 +1,63 @@
+#include <math.h>
+#include <stdio.h>
+#include <err.h>
+#include <SDL/SDL.h>
+
+//in case we have cb cr so that we modify rgb values directly
+void ycbcr_to_bw(Uint8 *r, Uint8 *g, Uint8 *b, int *cb, int *cr)
+{
+    //in YcBcR, Y (lux) is not important, only cr and cb
+    if ((*cb>100 && *cb<127) && (*cr>130 && *cr<175))
+    {
+        *r = 255;
+        *g = 255;
+        *b = 255; 
+    }
+    else
+    {
+        *r = 0;
+        *g = 0;
+        *b = 0;
+    } 
+}
+
+//in case we do not have CB CR, but we have rgb
+void ycbcr_to_bw_vrgb(Uint8 *r, Uint8 *g, Uint8 *b)
+{
+    if ((*r<215 && *g<120 && *b <163) && 
+    (*r>160 && *g>112 && *b>140))
+    {
+        *r = 255;
+        *g = 255;
+        *b = 255; 
+    }
+    else
+    {
+        *r = 0;
+        *g = 0;
+        *b = 0;
+    } 
+       
+}
+
+
+int main(void)
+{
+    int cb =101;
+    int cr = 131;
+
+    Uint8 r =161;
+    Uint8 g = 114;
+    Uint8 b = 141;
+
+
+    //testing
+    ycbcr_to_bw(&r,&g,&b,&cb,&cr);
+    printf("%ud ", r);
+
+    //testing
+    ycbcr_to_bw_vrgb(&r,&g,&b);
+    printf("%ud ", r);
+
+    return 0;
+}
