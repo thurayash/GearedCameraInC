a7be13659e38214abe7a16785595106fbeb72720
Yassine Damiri <yassine.damiri@epita.fr>
2021-03-02 18:44:02 +0100

    Web Interface with node.js configuration

---
+++ web/express/js/script.js
@@ -0,0 +1,16 @@
+function openTab(evt, tabName) {
+  var i, x, tablinks, label;
+  label = document.getElementById(tabName+"-label");
+  label.setAttribute("style", "color: black;");
+  x = document.getElementsByClassName("content-tab");
+  for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tab");
+  for (i = 0; i < x.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" is-active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " is-active";
+
+}
