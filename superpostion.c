1e34ca6e75edc8e945931aa68d719d854aad8f2a
karim abou dargham <karim.abou-dargham@epita.fr>
2021-03-04 21:14:40 +0100

    made superposition a bit more precise in coding

--- superpostion.c
+++ superpostion.c
@@ -2,49 +2,70 @@
 #include <stdio.h>
 #include <err.h>
 #include <SDL/SDL.h>
+#include <pixel.h> //this may be tools in the master branch
+
+
+
+void set_pixel(SDL_Surface *surface, int x, int y, Uint32 pixel)
+{
+  Uint32 *target_pixel = (Uint8 *) surface->pixels + y * surface->pitch +
+                                                     x * sizeof *target_pixel;
+  *target_pixel = pixel;
+}
+
 
 //majority sort : if two pictures indicate pixel[i][j] == 1, it is a face pixel
-int[][] superpostion1(int[][] image1, int[][] image2, int[][] image3)
+void superpostion1(SDL_Surface* image1, SDL_Surface*  image2, SDL_Surface* image3)
 {
-    int res[image1->width][image1->height]; //resultant image
-    for (int i = 0; i<image1->width; i++)
+    SDL_LockSurface(image1);
+    SDL_LockSurface(image2);
+    SDL_LockSurface(image3);
+    Uint32 black = 0xff;
+    Uint32 white = 0x00;
+    for (int i = 0; i<image1->w; i++)
     {
-        for (int j = 0; j<image1->height; j++)
+        //no need to check if all are 255 or 0 since this is after BW
+        for (int j = 0; j<image1->h; j++)
         {
-            if (image1[i][j] == 1) // or 255 if whiteblack pixel info
+            Uint32 pixel1 = get_pixel(image1, i, j);
+            Uint32 pixel2 = get_pixel(image2, i, j);
+            Uint32 pixel3 = get_pixel(image3, i, j);
+            if (pixel1== white) // or 255 if whiteblack pixel info instead of binary
             {
-                if (image2[i][j] == 1 || image3[i][j] == 1)
+                if (pixel2 == white || pixel3 == white)
                 {
-                    res[i][j] = 1;
+                    set_pixel(*image1, i,j,white);
                 }
             }
-            else if (image2[i][j] == 1) //or 255 if we go whiteblack)
+            else if (pixel2 == white && pixel3 == white) //or 255 if we go whiteblack)
             {
-                if (image3[i][j] == 1)
-                {
-                    res[i][j] = 1;
-                }
+                set_pixel(*image1, i,j,white)
             }
         }
     }
-    return res;
 }
 
 //selective sort : if all 3 pictures indicate pixel[i][j] == 1, it is a face pixel
-int[][] superpostion2(int[][] image1, int[][] image2, int[][] image3)
+void superpostion2(SDL_Surface* image1, SDL_Surface*  image2, SDL_Surface* image3)
 {
-    int res[image1->width][image1->height]; //resultant image
-    for (int i = 0; i<image1->width; i++)
+    SDL_LockSurface(image1);
+    SDL_LockSurface(image2);
+    SDL_LockSurface(image3);
+    Uint32 black = 0xff;
+    Uint32 white = 0x00;
+    for (int i = 0; i < image1->w; i++)
     {
-        for (int j = 0; j<image1->height; j++)
+        for (int j = 0; j < image1->h; j++)
         {
-            if (image1[i][j] == 1 
-                && image2[i][j] == 1 
-                && image3[i][j] ==1)
+            Uint32 pixel1 = get_pixel(image1, i, j);
+            Uint32 pixel2 = get_pixel(image2, i, j);
+            Uint32 pixel3 = get_pixel(image3, i, j);
+            if (pixel1 == white
+                && image2[i][j] == white
+                && image3[i][j] ==white)
             {
-                res[i][j]= 1;
+                set_pixel(*image1,i,j,white)
             }
         }
     }
-    return res;
 }
