7f3775a62e11b8d6d50e919f7c018e0b90604c19
karim abou dargham <karim.abou-dargham@epita.fr>
2021-03-06 22:21:50 +0100

    forgot a small ;

--- superpostion.h
+++ superpostion.h
@@ -4,6 +4,6 @@
 #include <SDL/SDL.h>
 #include <pixel.h>
 void superpostion1(SDL_Surface* image1, SDL_Surface*  image2, SDL_Surface* image3);
-void superpostion2(SDL_Surface* image1, SDL_Surface*  image2, SDL_Surface* image3)
+void superpostion2(SDL_Surface* image1, SDL_Surface*  image2, SDL_Surface* image3);
 
 #endif
