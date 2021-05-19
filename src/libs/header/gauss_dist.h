9811108e6e4fe9d5861fd344040afcdcaa158fe4
Maxence Crouzy <maxence.crouzy@epita.fr>
2021-05-19 15:18:13 +0200

    Gaussian distance

---
+++ src/libs/header/gauss_dist.h
@@ -0,0 +1,11 @@
+#ifndef GAUSS_DIST_H_
+#define GAUSS_DIST_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int gauss_dist(int id, int x1, int y1, int x2, int y2);
+double* find_angle(int x1, int y1, int x2, int y2, int dist1, int dist2);
+
+#endif
