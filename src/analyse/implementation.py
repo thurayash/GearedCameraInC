b36c9595d44ffc3f02dcecc6be101e42da8460bb
thurayaonubuntu <thuraya.shanbari@epita.fr>
2021-05-22 16:43:49 +0200

    wasserstein SOMETHING but idk if it works yet

--- src/analyse/implementation.py
+++ src/analyse/implementation.py
@@ -1,12 +1,15 @@
 # Do the python implementation here !
 
+
 def kolmogorov(histo1, histo2):
     stat, p_value = 0,0
     # TODO
     return (stat, p_value)
 
 
+
 def wasserstein(histo1, histo2):
     distance = 0
-    #TODO
+    for i in range(len(histo1)):
+        distance = max(distance, abs(histo1[i] - histo2[i]))
     return distance
