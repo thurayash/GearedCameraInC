df08a62bc731322ea33277c317f31ed4588cdff9
Yassine Damiri <yassine.damiri@epita.fr>
2021-05-22 04:51:08 +0200

    Python env setup !

---
+++ src/analyse/implementation.py
@@ -0,0 +1,12 @@
+# Do the python implementation here !
+
+def kolmogorov(histo1, histo2):
+    stat, p_value = 0,0
+    # TODO
+    return (stat, p_value)
+
+
+def wasserstein(histo1, histo2):
+    distance = 0
+    #TODO
+    return distance
